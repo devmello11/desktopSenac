7836153361b27f3d7e032dd5ea1a1f6e9f267d75
Robert Mello <robertale45@gmail.com>
2025-06-11 10:57:56 -0300

    add nova tela conectando mysql

---
+++ desktopSenac-main/JavaSenac/src/BancoDeDados/ConexaoMySql.java
@@ -0,0 +1,44 @@
+package BancoDeDados;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.awt.Component;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+/**
+ *
+ * @author Robert51816586
+ */
+public class ConexaoMySql {
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+      private static final String URL = "jdbc:mysql://localhost:3306/bancode dados";
+      private static final String USER = "root";
+      private static final String PASS = "senac";
+   
+   
+    public static Connection conexaoBanco() throws SQLException {
+       
+       
+        try {
+             Class.forName(DRIVER);
+             Component parentComponent = null;
+                 
+             
+             
+             return DriverManager.getConnection(URL,USER, PASS);
+           
+         } catch (ClassNotFoundException ex) {
+            Component parentComponent = null;
+           
+             
+      throw new RuntimeException("Erro no banco de dados",ex);
+       
+         }
+       
+       
+       
+    }
+}
